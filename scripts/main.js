842f0a3369f1b4c91e30445da90443a6140bac24
Isabel <isabel.antonia.seidl@gmail.com>
2019-01-30 00:44:14 +0100

    redone version

--- scripts/main.js
+++ scripts/main.js
@@ -1,46 +1,36 @@
-// fetch 
-// both vue and js
-// create the template 
-// in the template use vue 
 
 
 
-var myVue = new Vue({
-    el: '#implementVue',
-    data: {
-        bookarray: [],
-        inputValue: '',
-        filteredBooks: [],
-        myWindow: {
-            width: 0,
-            height: 0
-        },
-        noResults: false,
-        yourInput: ''
+var app = new Vue({
+  el: '#app',
+  data: {
+    bookArray: {},
+      userInput: '',
+      filteredBooks: [],
+      userInputNew: '',
+      noResults: false
+  },
+    
+    
+    beforeCreate(){
+        
+        fetch('https://api.myjson.com/bins/8zpvs')
+        .then(response => response.json())
+        .then(data => {
+            this.bookArray = data.books;
+            
+            this.displayBooks();
+            this.box();
+            
+        })
+   
+             
+        
     },
-
-    beforeCreate() {
-
-        fetch("https://api.myjson.com/bins/8zpvs")
-            .then(response => response.json())
-            .then(realData => {
-
-                this.bookarray = realData.books;
-                this.box();
-                this.searchFilter();
-                        
-
-            })
-
-    },
-//    created()  {
-//                    window.addEventListener('resize', this.handleResize)
-//                    this.handleResize();
-//                
-//    },
+    
     methods: {
-
-        box: function () {
+        
+                box: function () {
 
             $('.swipebox').swipebox({
                 useCSS: true, // false will force the use of jQuery for animations
@@ -56,137 +46,31 @@ var myVue = new Vue({
                 loopAtEnd: false // true will return to the first image after the last image is reached
             });
         },
-
-        searchFilter: function () {
+        
+        
+        displayBooks(){
+            
             this.noResults = false;
-            this.yourInput = this.inputValue.toUpperCase();
             
-
+            this.userInputNew = this.userInput.toUpperCase();
             
-            this.filteredBooks = this.bookarray.filter(book => {
-                return JSON.stringify(book).toUpperCase().includes(this.yourInput)
-                
-                
-           
-                
-                // only returns the books that pass the test
-                // if there is no User Input it applies to all, because all books have empty spaces
-                
-            })
+            this.filteredBooks = this.bookArray.filter(book => JSON.stringify(book).toUpperCase().includes(this.userInputNew))
             
-       if (this.filteredBooks.length == 0){
-           this.noResults = true;
-           
-       }
+            console.log(this.filteredBooks);
             
-
-           }
-
-          
-
-
-//        
-//        handleResize: function () {
-//                            this.myWindow.width = window.innerWidth;
-//                            this.myWindow.height = window.innerHeight;
-//                        }
-
-
+            
+            if (this.filteredBooks.length === 0){
+                this.noResults = true;
+            }
+            
+            
+        }
     }
+    
+    
+    
+    
+    
+    
+    
 })
-
-
-
-
-
-
-
-
-
-// in plain js
-
-//var bookarray;
-//
-//onload = (function () {
-//
-//    fetch("https://api.myjson.com/bins/8zpvs")
-//        .then(response => response.json())
-//        .then(realData => {
-//        
-//        bookarray = realData.books;
-//            printBooks(bookarray)
-//        })
-//})
-//
-//// in Vue 
-//
-////var app = new Vue({
-////  el: '#here',
-////  data: {
-////    message: 'Hello Vue!'
-////  }
-////})
-//
-//function printBooks(data){
-//    var template="";
-//    
-//    data.forEach(book => {
-//                template += ` <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-//                                <div class="flipper">
-//
-//                                    <div class="front">
-//                                        <img id="img" src="${book.cover}">
-//                                    </div>
-//
-//                                    <div class="back p-4">
-//                                        <h3>${book.title}</h3>
-//                                        <p>${book.description}</p>
-//
-//                                    <a href=${book.detail} class="swipebox">
-//                                        <button class="btn btn-info">More Info</button>
-//                                    </a>
-//                                    </div>
-//                                </div>
-//                            </div>`
-//    })
-//    document.querySelector("#here").innerHTML = template;
-//    box();
-//    
-///*    console.log($("a.images"));
-//    $("a.images").fancybox({
-//		'transitionIn'	:	'elastic',
-//		'transitionOut'	:	'elastic',
-//		'speedIn'		:	600, 
-//		'speedOut'		:	200, 
-//		'overlayShow'	:	false
-//	});*/
-//}
-//
-//function box() {
-//
-//        $( '.swipebox' ).swipebox( {
-//            useCSS : true, // false will force the use of jQuery for animations
-//            useSVG : true, // false to force the use of png for buttons
-//            initialIndexOnArray : 0, // which image index to init when a array is passed
-//            hideCloseButtonOnMobile : false, // true will hide the close button on mobile devices
-//            removeBarsOnMobile : true, // false will show top bar on mobile devices
-//            hideBarsDelay : 3000, // delay before hiding bars on desktop
-//            videoMaxWidth : 1140, // videos max width
-//            beforeOpen: function() {}, // called before opening
-//            afterOpen: null, // called after opening
-//            afterClose: function() {}, // called after closing
-//            loopAtEnd: false // true will return to the first image after the last image is reached
-//        } );
-//};
-//
-//function searchFilter(){
-//   input = document.getElementById('here');
-//   filter = document.getElementById("quicksearch").value.toUpperCase();
-////   console.log(filter);
-//
-//   let filteredBooks = bookarray.filter((book)=>{return JSON.stringify(book).toUpperCase().includes(filter) >0})
-//
-//   printBooks(filteredBooks)
-//
-//
-//}
